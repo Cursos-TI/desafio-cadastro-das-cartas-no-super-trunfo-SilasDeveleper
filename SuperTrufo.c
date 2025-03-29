dcef92e1cf3e0e04c579359ead50809c2d761abb
SilasDeveleper <silaslindososerra1001@gmail.com>
2025-03-28 21:35:04 -0300

    Desafio nível aventureiro

--- SuperTrufo.c
+++ SuperTrufo.c
@@ -7,8 +7,8 @@ int main(){
 
     setlocale(LC_ALL, "Portuguese_Brazil");
 
-    char estado, estado1;
-    char codigo[5], codigo1[5];// Array de caracteres para armazenar o código (ex: "A01" + '\0')
+    char estado[3], estado1[3];
+    char codigo[4], codigo1[4];// Array de caracteres para armazenar o código (ex: "A01" + '\0')
     char nomecidade[50], nomecidade1[50];// Array de caracteres para armazenar o nome da cidade
     int populacao, populacao1;
     float area, area1;
@@ -22,12 +22,12 @@ int main(){
     printf("Carta numero 1\n");
 
     //Estado
-    printf("Digite a primeira letra do Estado (A-H): ");
-    scanf(" %c", &estado);
+    printf("Digite a sigla do Estado (ex: MA, PA, SP): ");
+    scanf("%2s", estado);
 
     //Codigo da carta
     printf("Digite o código da carta (ex: A01, B04): ");
-    scanf("%4s", codigo);// "%4s" limita a entrada a 4 caracteres (evita estouro)
+    scanf("%3s", codigo);// "%3s" limita a entrada a 4 caracteres (evita estouro)
 
     //limpa Limpar buffer do teclado antes de usar `fgets`
     getchar();
@@ -53,16 +53,20 @@ int main(){
     printf("Digite o número de pontos turisticos: ");
     scanf("%d", &pontosturisticos);
 
+    float densidade = (float)(populacao / area);
+    float pibpercapita = (float)(PIB / populacao);
+
     //Exibir dados:
     printf("\n=== Carta 1 ===\n");
-    printf("Estado: %c\n", estado);
+    printf("Estado: %2s\n", estado);
     printf("Codigo da Carta: %s\n", codigo);
     printf("Nome da cidade: %s\n", nomecidade);
     printf("população: %d habitantes\n", populacao);
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f Bilhões de reais\n", PIB);
     printf("Pontos Turisticos: %d\n", pontosturisticos);
-
+    printf("Densidade Populacional: %.2f Hab/Km²\n", densidade);
+    printf("PIB per Capita: %.2f reias\n", pibpercapita);
 
     //Inserir a segunda carta
     printf("===================================\n");
@@ -71,12 +75,12 @@ int main(){
     printf("Carta numero 2\n");
 
     //Estado
-    printf("Digite a primeira letra do Estado (A-H): ");
-    scanf(" %c", &estado1);
+    printf("Digite a sigla do Estado (ex: MA, PA, SP): ");
+    scanf("%2s", estado1);
 
     //Codigo da carta
     printf("Digite o código da carta (ex: A01, B04): ");
-    scanf("%4s", codigo1);// "%4s" limita a entrada a 4 caracteres (evita estouro)
+    scanf("%3s", codigo1);// "%3s" limita a entrada a 4 caracteres (evita estouro)
 
     //limpa Limpar buffer do teclado antes de usar `fgets`
     getchar();
@@ -101,17 +105,23 @@ int main(){
     //Pontos turisticos
     printf("Digite o número de pontos turisticos: ");
     scanf("%d", &pontosturisticos1);
+    
+    float densidade1 = (float)(populacao1 / area1);
+    float pibpercapita1 = (float)(PIB1 / populacao1);
 
     //Exibir dados:
     printf("\n=== Carta 2 ===\n");
-    printf("Estado: %c\n", estado1);
+    printf("Estado: %s\n", estado1);
     printf("Codigo da Carta: %s\n", codigo1);
     printf("Nome da cidade: %s\n", nomecidade1);
     printf("população: %d habitantes\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f Bilhões de reais\n", PIB1);
     printf("Pontos Turisticos: %d\n", pontosturisticos1);
-  return 0;
+    printf("Densidade Populacional: %.2f Hab/Km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibpercapita1);
 
-  //que trabalheira para conectar o github no vscode
+  // densidade = populção/area
+  // PIB percapita = PIB/população
+  return 0;
 }
